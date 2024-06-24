56b06ae47e4f6d19b1d3257b07cbe906bb5b40a6
vishnu1408-prog <vishnusreedhar2001@gmail.com>
2024-06-24 18:29:42 +0530

    button closing function

--- app.py
+++ app.py
@@ -89,7 +89,6 @@ def handle_form_submission():
         if not os.path.exists(upload_folder):
             os.makedirs(upload_folder)
 
-
         file_upload.save(os.path.join(upload_folder, filename))
         
         # Simulating validation process (write to simulation.txt)
@@ -101,7 +100,6 @@ def handle_form_submission():
             with open('simulation/simulation.txt', 'r') as f:
                 content = f.read()
                 if content == "0":
-                    os.system("rm -rf output/*")
                     break
                 time.sleep(1)
 
