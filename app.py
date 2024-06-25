effe536212f808b3828f2b67afa478e47f4e7a65
gokulcountonai <gokul@counton.ai>
2024-06-25 14:28:14 +0530

    rollback to redis disconnecion

--- app.py
+++ app.py
@@ -9,110 +9,136 @@ image_processor = ImageProcessor()
 app = Flask(__name__)
 
 def run_validation(path, fps, score):
-    image_processor.path = path
-    image_processor.score = float(score)
-    return image_processor.read_images(path, fps)
+    try:
+        image_processor.path = path
+        image_processor.score = float(score)
+        return image_processor.read_images(path, fps)
+    except Exception as e:
+        print("Error in reading images:", e)
+        return False
 
 @app.route('/machines_by_mill/<mill_name>', methods=['GET'])
 def get_machines_by_mill(mill_name):
-    if mill_name:
-        machine_details = db.fetch_machine_details_by_mill_name(mill_name)
-        return jsonify(machine_details)
-    return jsonify([])
+    try:
+        if mill_name:
+            machine_details = db.fetch_machine_details_by_mill_name(mill_name)
+            return jsonify(machine_details)
+        return jsonify([])
+    except Exception as e:
+        print(str(e))
+        return jsonify({"status": "error", "message": str(e)}), 400
 
 @app.route('/mills', methods=['GET'])
 def get_mill_details():
-    mill_details = db.fetch_mill_details()
-    return jsonify(mill_details)
+    try:
+        mill_details = db.fetch_mill_details()
+        return jsonify(mill_details)
+    except Exception as e:
+        print(str(e))
+        return jsonify({"status": "error", "message": str(e)}), 400
 
 @app.route('/add-machine', methods=['POST'])
 def add_machine():
-    data = request.json
-    mill_name = data.get('mill_name')
-    machine_name = data.get('machine_name')
-    if not mill_name or not machine_name:
-        return jsonify({"status": "error", "message": "Please provide both mill name and machine name."}), 400
+    try:
+        data = request.json
+        mill_name = data.get('mill_name')
+        machine_name = data.get('machine_name')
+        if not mill_name or not machine_name:
+            return jsonify({"status": "error", "message": "Please provide both mill name and machine name."}), 400
 
-    mill_id = db.fetch_mill_details_by_millname(mill_name)
-    if not mill_id:
-        return jsonify({"status": "error", "message": "Mill name not found. Please ADD the mill first."}), 400
+        mill_id = db.fetch_mill_details_by_millname(mill_name)
+        if not mill_id:
+            return jsonify({"status": "error", "message": "Mill name not found. Please ADD the mill first."}), 400
 
-    data = {
-        "machine_name": machine_name,
-        "mill_id": mill_id["mill_id"]
-    }
-    db.add_machine(data)
+        data = {
+            "machine_name": machine_name,
+            "mill_id": mill_id["mill_id"]
+        }
+        db.add_machine(data)
 
-    return jsonify({"status": "success", "message": "Machine added successfully."})
+        return jsonify({"status": "success", "message": "Machine added successfully."})
+    except Exception as e:
+        print(str(e))
+        return jsonify({"status": "error", "message": str(e)}), 400
 
 @app.route('/submit-form', methods=['POST'])
 def handle_form_submission():
-    mill_name = request.form.get('mill')
-    machine_name = request.form.get('machine')
-    simulation_type = request.form.get('validationType')
-    folder_path = request.form.get('folderpath')
-    file_upload = request.files['fileUpload']
-    score = request.form.get('score')
-    fps = request.form.get('fps')
-    report = request.form.get('report')
-
     try:
-        score_float = float(score)
-        fps_float = float(fps)
+        mill_name = request.form.get('mill')
+        machine_name = request.form.get('machine')
+        simulation_type = request.form.get('validationType')
+        folder_path = request.form.get('folderpath')
+        file_upload = request.files['fileUpload']
+        score = request.form.get('score')
+        fps = request.form.get('fps')
+        report = request.form.get('report')
 
-        if not (0 <= score_float <= 1.0):
-            return jsonify({"status": "error", "message": "Score must be between 0 and 1."}), 400
-        
-        if not (10 <= fps_float <= 40):
-            return jsonify({"status": "error", "message": "FPS must be between 10 and 40."}), 400
+        try:
+            score_float = float(score)
+            fps_float = float(fps)
 
-    except ValueError:
-        return jsonify({"status": "error", "message": "Invalid score or fps value."}), 400
+            if not (0 <= score_float <= 1.0):
+                return jsonify({"status": "error", "message": "Score must be between 0 and 1."}), 400
+            
+            if not (10 <= fps_float <= 40):
+                return jsonify({"status": "error", "message": "FPS must be between 10 and 40."}), 400
 
-    data = {
-        "mill": mill_name,
-        "machine": machine_name,
-        "simulation_type": simulation_type,
-        "score": score,
-        "fps": fps,
-        "report_availability": report,
-        "fileUpload": file_upload,
-        "folderpath": folder_path
-    }
+        except ValueError:
+            return jsonify({"status": "error", "message": "Invalid score or fps value."}), 400
 
-    db.insert_validation_log(data)
+        data = {
+            "mill": mill_name,
+            "machine": machine_name,
+            "simulation_type": simulation_type,
+            "score": score,
+            "fps": fps,
+            "report_availability": report,
+            "fileUpload": file_upload,
+            "folderpath": folder_path
+        }
 
-    if file_upload:
-        filename = "reg"
-        upload_folder = "./uploads"
+        db.insert_validation_log(data)
 
-        if not os.path.exists(upload_folder):
-            os.makedirs(upload_folder)
+        if file_upload:
+            filename = "reg"
+            upload_folder = "./uploads"
 
-        file_upload.save(os.path.join(upload_folder, filename))
-        
-        # Simulating validation process (write to simulation.txt)
-        with open('simulation/simulation.txt', 'w') as f:
-            f.write("1")
+            if not os.path.exists(upload_folder):
+                os.makedirs(upload_folder)
 
-        time.sleep(5)
-        while True:
-            with open('simulation/simulation.txt', 'r') as f:
-                content = f.read()
-                if content == "0":
-                    break
-                time.sleep(1)
 
-        run_validation(folder_path, fps, score)
+            file_upload.save(os.path.join(upload_folder, filename))
+            
+            # Simulating validation process (write to simulation.txt)
+            with open('simulation/simulation.txt', 'w') as f:
+                f.write("1")
 
-    return jsonify({"status": "success", "message": "Validation started successfully", "report": report})
+            time.sleep(5)
+            while True:
+                with open('simulation/simulation.txt', 'r') as f:
+                    content = f.read()
+                    if content == "0":
+                        os.system("rm -rf output/*")
+                        break
+                    time.sleep(1)
+
+            run_validation(folder_path, fps, score)
+
+        return jsonify({"status": "success", "message": "Validation started successfully", "report": report})
+    except Exception as e:
+        print(str(e))
+        return jsonify({"status": "error", "message": str(e)}), 400
 
 @app.route('/add-mill', methods=['POST'])
 def add_mill():
-    data = request.json
-    mill_name = data.get('name').capitalize() 
-    add_millname = db.insert_millname({"name": mill_name})
-    return jsonify(add_millname)
+    try:
+        data = request.json
+        mill_name = data.get('name').capitalize() 
+        add_millname = db.insert_millname({"name": mill_name})
+        return jsonify(add_millname)
+    except Exception as e:
+        print(str(e))
+        return jsonify({"status": "error", "message": str(e)}), 400
 
 @app.route('/')
 def index():
--- main.py
+++ main.py
@@ -8,12 +8,15 @@ from src.storeimages import StoreImage
 
 class ImageProcessor:
     def __init__(self):
-        self.storeimages = StoreImage()
-        self.inference = Inference()
-        self.score = 0.1
-        self.path = ""
-        thread = threading.Thread(target=self.get_infer_result)
-        thread.start()
+        try:
+            self.storeimages = StoreImage()
+            self.inference = Inference()
+            self.score = 0.1
+            self.path = ""
+            thread = threading.Thread(target=self.get_infer_result)
+            thread.start()
+        except Exception as e:
+            print(str(e))
 
     def read_images(self, path, fps, camtype="1"):
         try:
@@ -31,65 +34,59 @@ class ImageProcessor:
 
     def get_infer_result(self):
         while True:
-            ret, data = self.inference.getInferResult()
-            # print(data)
-            if ret != False:
-                print(data)
-                if data["classType"] == "None":
-                    continue
+            try:
+                ret, data = self.inference.getInferResult()
+                # print(data)
+                if ret != False:
+                    print(data)
+                    if data["classType"] == "None":
+                        continue
 
-                classType = [n.strip() for n in ast.literal_eval(data["classType"])]
-                conf = [n for n in ast.literal_eval(data["conf"])]
-                xyxy = [n for n in ast.literal_eval(data["xyxy"])]
-                imageId = data["imageId"]
+                    classType = [n.strip() for n in ast.literal_eval(data["classType"])]
+                    conf = [n for n in ast.literal_eval(data["conf"])]
+                    xyxy = [n for n in ast.literal_eval(data["xyxy"])]
+                    imageId = data["imageId"]
 
-                print(self.path + "/" + imageId[2:])
-                img = cv2.imread(self.path + imageId[2:])
-                unbox = img.copy()
+                    print(self.path + "/" + imageId[2:])
+                    img = cv2.imread(self.path + imageId[2:])
+                    unbox = img.copy()
 
-                for i, cls in enumerate(classType):
-                    print("Class:", cls)
-                    print("Confidence:", conf[i])
-                    print("Bounding Box:", xyxy[i])
-                    print("Image Id:", imageId)
+                    for i, cls in enumerate(classType):
+                        print("Class:", cls)
+                        print("Confidence:", conf[i])
+                        print("Bounding Box:", xyxy[i])
+                        print("Image Id:", imageId)
 
-                    if conf[i] > self.score:
-                        os.makedirs("output", exist_ok=True)
-                        os.makedirs("output/box/", exist_ok=True)
-                        os.makedirs("output/unbox/", exist_ok=True)
+                        if conf[i] > self.score:
+                            os.makedirs("output", exist_ok=True)
+                            os.makedirs("output/box/", exist_ok=True)
+                            os.makedirs("output/unbox/", exist_ok=True)
 
-                        os.makedirs("output/box/" + cls, exist_ok=True)
-                        os.makedirs("output/unbox/" + cls, exist_ok=True)
+                            os.makedirs("output/box/" + cls, exist_ok=True)
+                            os.makedirs("output/unbox/" + cls, exist_ok=True)
 
-                        x1, y1, x2, y2 = xyxy[i]
-                        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                        cv2.putText(img, cls, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-                        cv2.putText(img, str(conf[i]), (x1, y1 - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                            x1, y1, x2, y2 = xyxy[i]
+                            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                            cv2.putText(img, cls, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                            cv2.putText(img, str(conf[i]), (x1, y1 - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
-                        jsonlocation = "output/" + cls + "/"
-                        print(jsonlocation)
-                        os.makedirs(jsonlocation, exist_ok=True)
-                        print(imageId)
-                        self.storeimages.setLabel(
-                            img.copy(),
-                            str(conf[i]),
-                            xyxy[i],
-                            jsonlocation + imageId,
-                            500,
-                            10,
-                        )
+                            jsonlocation = "output/" + cls + "/"
+                            print(jsonlocation)
+                            os.makedirs(jsonlocation, exist_ok=True)
+                            print(imageId)
+                            self.storeimages.setLabel(
+                                img.copy(),
+                                str(conf[i]),
+                                xyxy[i],
+                                jsonlocation + imageId,
+                                500,
+                                10,
+                            )
 
-                        cv2.imwrite("output/box/" + cls + "/" + imageId + "_" + str(conf[i]) + ".png", img)
-                        cv2.imwrite("output/unbox/" + cls + "/" + imageId + str(conf[i]) + ".png", unbox)
+                            cv2.imwrite("output/box/" + cls + "/" + imageId + "_" + str(conf[i]) + ".png", img)
+                            cv2.imwrite("output/unbox/" + cls + "/" + imageId + str(conf[i]) + ".png", unbox)
+            except Exception as e:
+                print(str(e))
 
             time.sleep(0.005)
-
-# # Usage example:
-# path = sys.argv[1]
-# camtype = sys.argv[2]
-# fps = sys.argv[3]
-
-# image_processor = ImageProcessor()
-# image_processor.read_images(path, camtype, fps)
-# image_processor.get_infer_result(path, camtype, fps)
