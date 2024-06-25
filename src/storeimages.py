790b5bb85f66b61b050e371c2ee7d1f2eab4b5f4
gokulcountonai <gokul@counton.ai>
2024-06-25 15:20:58 +0530

    initial push after conflicts

--- src/storeimages.py
+++ src/storeimages.py
@@ -5,25 +5,33 @@ import threading
 from src.label import Label, Shape
 
 class StoreImage:
+    """
+    Class to store and process images.
+    """
+
     def __init__(self):
+        """
+        Initialize the StoreImage class.
+        """
         self.imageQueue = queue.Queue(maxsize=50)
-        self.thread = threading.Thread(target=self.storeimage)
+        self.thread = threading.Thread(target=self.store_image)
         self.thread.start()
         self.label = Label()
         self.shape = Shape()
 
-    def storeimage(self):
+    def store_image(self):
+        """
+        Process and store images from the image queue.
+        """
         while True:
             try:
                 if self.imageQueue.qsize() > 0:
-                    image,location,label = self.imageQueue.get()
-                    if label == True:
+                    image, location, label = self.imageQueue.get()
+                    if label:
                         for cb in image:
                             cb[0](cb[1])
-                    
                     else:
-                        cv2.imwrite(location,image)
-
+                        cv2.imwrite(location, image)
                 else:
                     time.sleep(0.01)
             except Exception as e:
@@ -31,36 +39,41 @@ class StoreImage:
                 time.sleep(0.01)
                 continue
 
-    def setImage(self,image,location):
-        self.imageQueue.put([image,location,False])
+    def set_image(self, image, location):
+        """
+        Add an image to the image queue.
+        """
+        self.imageQueue.put([image, location, False])
         return True
 
-    def setDefectImage(self,image,location):
-        self.imageQueue.put([image,location,False])
+    def set_defect_image(self, image, location):
+        """
+        Add a defect image to the image queue.
+        """
+        self.imageQueue.put([image, location, False])
         return True
 
-    def setLabel(self,image,result,coordinates,location,exposure,gain):  
-        #create a callback to save the label
+    def set_label(self, image, result, coordinates, location, exposure, gain):
+        """
+        Add a label to the image and save it.
+        """
         self.label = Label()
         self.shape = Shape()
         cb = []
-        cb.append([self.shape.add_label,result])
-        x1,y1,x2,y2 = coordinates
-        x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
-        cb.append([self.shape.add_points,[[x1,y1],[x2,y2]]])
-        cb.append([self.shape.add_shape_type,'rectangle'])
-        cb.append([self.label.add_shapes,self.shape])
-        cb.append([self.label.add_exposure,exposure])
-        cb.append([self.label.add_gain,gain])
-        
-        cb.append([self.label.add_imagePath,location.split("/")[-1]])
-        cb.append([self.label.setImage,image])
-        location = location[:-3]+"json"
-        cb.append([self.label.saveLabel,location])
-        if self.imageQueue.full()  != True:
-            self.imageQueue.put([cb,"",True])
-        del(self.label)
-        del(self.shape)
+        cb.append([self.shape.add_label, result])
+        x1, y1, x2, y2 = coordinates
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        cb.append([self.shape.add_points, [[x1, y1], [x2, y2]]])
+        cb.append([self.shape.add_shape_type, 'rectangle'])
+        cb.append([self.label.add_shapes, self.shape])
+        cb.append([self.label.add_exposure, exposure])
+        cb.append([self.label.add_gain, gain])
+        cb.append([self.label.add_imagePath, location.split("/")[-1]])
+        cb.append([self.label.set_image, image])
+        location = location[:-3] + "json"
+        cb.append([self.label.saveLabel, location])
+        if not self.imageQueue.full():
+            self.imageQueue.put([cb, "", True])
+        del self.label
+        del self.shape
         return True
-
-     
